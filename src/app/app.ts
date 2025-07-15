066c9ef80b93d0c7214ac1ced8db13333c657cd5
lakma <lakmalaberathne@gmail.com>
2025-07-15 20:10:05 +0530

    feat : added login page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './pages/header/header';
+import { Footer } from './pages/footer/footer';
+import { Home } from './pages/home/home';
+import { Login } from "./pages/login/login";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer, Login],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
